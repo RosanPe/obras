3a216451319bb1b1685f07242beb9eef7680e887
RosanPe <pedro.rosan@cs.unifran.edu.br>
2026-06-12 17:59:27 -0300

    Add files via upload

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  supabaseUrl: "https://tjjixejbflcjopevogow.supabase.co/rest/v1/",
+  supabaseUrl: "https://tjjixejbflcjopevogow.supabase.co",
   supabaseAnonKey: "sb_publishable_jC9YeWZvN_eihgwCL_jxeg_41FkYTH5",
   baseId: "principal"
 };
